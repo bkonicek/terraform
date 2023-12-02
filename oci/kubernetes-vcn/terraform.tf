24a993a705e80956cd8c8fa6a598a92e9d21aa8c
Ben Konicek <ben.konicek@gmail.com>
2023-12-01 22:08:43 -0500

    Update terraform providers

--- oci/kubernetes-vcn/terraform.tf
+++ oci/kubernetes-vcn/terraform.tf
@@ -1,8 +1,9 @@
 terraform {
-  required_version = "~> 1.3"
+  required_version = ">= 1.0"
   required_providers {
     oci = {
-      source = "oracle/oci"
+      source  = "oracle/oci"
+      version = ">= 5.0"
     }
   }
 
