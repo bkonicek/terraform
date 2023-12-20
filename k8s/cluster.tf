07b140e766b7a5f83092ffdb4afffb6f36bbaad7
Benjamin Konicek <ben.konicek@gmail.com>
2023-12-20 11:54:28 -0500

    chore: Delete unused k8s tf directory

--- k8s/cluster.tf
+++
@@ -1,8 +0,0 @@
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "oci"
-}
-
-provider "oci" {
-  region = var.region
-}
--- k8s/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-org_name = "dev-02634852"
-base_url = "okta.com"
