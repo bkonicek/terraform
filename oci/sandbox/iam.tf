f9ee948f3891211ef047e5c4ed315cee8b2c969f
Ben Konicek <ben.konicek@gmail.com>
2024-07-02 08:39:27 -0400

    import existing grafana_policy

--- oci/sandbox/iam.tf
+++ oci/sandbox/iam.tf
@@ -1,21 +1,32 @@
-# Cluster-autoscaler IAM
-resource "oci_identity_dynamic_group" "cluster_autoscaler" {
+resource "oci_identity_dynamic_group" "oke_nodes" {
   compartment_id = var.compartment_id
-  description    = "Dynamic group for OKE Cluster autoscaler"
+  description    = "Dynamic group for all OKE Cluster nodes"
   matching_rule  = "All {instance.compartment.id = '${var.compartment_id}', tag.Oracle-Tags.CreatedBy.value='oke'}"
-  name           = "oke-cluster-autoscaler-dyn-group"
+  name           = "oke-dyn-group-all"
 }
 
+# Cluster-autoscaler IAM
 resource "oci_identity_policy" "autoscaler_manage_nodepools" {
   compartment_id = var.compartment_id
   description    = "Allow worker nodes to manage node pools for cluster autoscaler"
   name           = "worker-node-cluster-autoscaler-node-pool"
   statements = [
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler.name} to manage cluster-node-pools in tenancy",
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler.name} to manage instance-family in tenancy",
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler.name} to use subnets in tenancy",
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler.name} to read virtual-network-family in tenancy",
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler.name} to use vnics in tenancy",
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler.name} to inspect compartments in tenancy",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes.name} to manage cluster-node-pools in tenancy",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes.name} to manage instance-family in tenancy",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes.name} to use subnets in tenancy",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes.name} to read virtual-network-family in tenancy",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes.name} to use vnics in tenancy",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes.name} to inspect compartments in tenancy",
+  ]
+}
+
+# Metrics for Grafana
+resource "oci_identity_policy" "grafana_policy" {
+  compartment_id = var.compartment_id
+  description    = "Allow worker nodes to read infrastructure metrics"
+  name           = "grafana_policy"
+  statements = [
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes.name} to read metrics in tenancy",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes.name} to read compartments in tenancy",
   ]
 }
